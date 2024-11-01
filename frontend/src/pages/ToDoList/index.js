f109c736578773790f36bfc966e4b5feab6c358b
IancaCarregosa <Iancacarregosa@gmail.com>
2024-11-01 00:05:39 -0300

    Função nova de checkbox
    Nova função de checkbox para melhorar a organização do usuário

--- frontend/src/pages/ToDoList/index.js
+++ frontend/src/pages/ToDoList/index.js
@@ -7,6 +7,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import IconButton from '@material-ui/core/IconButton';
+import Checkbox from '@material-ui/core/Checkbox';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 
@@ -35,7 +36,10 @@ const useStyles = makeStyles({
   },
   list: {
     marginBottom: '5px'
-  }
+  },
+  checkbox: {
+    color: '#0C2C4C',
+  },
 });
 
 const ToDoList = () => {
@@ -110,6 +114,11 @@ const ToDoList = () => {
         <List>
           {tasks.map((task, index) => (
             <ListItem key={index} className={classes.list}>
+              <Checkbox
+                  className={classes.checkbox}
+                  size="small" // Tamanho pequeno
+                  edge="start"
+              />
               <ListItemText primary={task.text} secondary={task.updatedAt.toLocaleString()} />
               <ListItemSecondaryAction>
                 <IconButton onClick={() => handleEditTask(index)}>
