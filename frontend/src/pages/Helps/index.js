1ca7af6c42dd8e565b7a90522ef977ed760ef0e2
Luan Gouveia <luangfdev@gmail.com>
2024-11-11 13:49:03 -0300

    searchbarfunciona

--- frontend/src/pages/Helps/index.js
+++ frontend/src/pages/Helps/index.js
@@ -14,30 +14,39 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 const useStyles = makeStyles(theme => ({
   sidebarContainer: {
     width: '28%',
-    // Height: '100vh', // Altura máxima para preencher até a base da tela
     maxHeight: "100vh",
-    //  overflow: "hidden",
-    // overflowY: 'auto',  // Ativa a rolagem se o conteúdo ultrapassar o limite
-    // backgroundColor: '#EFF3F6',
     display: 'flex',
     flexDirection: 'column',
     padding: theme.spacing(2),
     boxSizing: "border-box",
   },
   searchInput: {
-    width: '98%',  // Largura do campo de pesquisa
-    borderRadius: 5,  // Borda de 5px
+    width: '96%',
+    borderRadius: 8,
     marginBottom: theme.spacing(2),
     backgroundColor: 'white',
     padding: theme.spacing(1),
   },
   titleList: {
     overflowY: 'auto',
-    maxHeight: '100vh',  // Ajusta a altura para permitir rolagem
+    maxHeight: '100vh',
     padding: theme.spacing(1),
     flex: 1,
     height: "calc(69vh + 1px)",
-    backgroundColor: "#EFF3F6"
+    backgroundColor: "#EFF3F6",
+    "&::-webkit-scrollbar": {
+      width: "7px",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: "#232323",
+      borderRadius: "20px",
+    },
+    "&::-webkit-scrollbar-track": {
+      backgroundColor: "transparent",
+    },
+    "&::-webkit-scrollbar-button": {
+      display: "none",
+    },
   },
   titleItem: {
     fontSize: '1.9rem',
@@ -54,25 +63,27 @@ const useStyles = makeStyles(theme => ({
   mainPaperContainer: {
     overflowY: 'auto',
     fontFamily: "Nunito, Arial, sans-serif",
-   
+    "&::-webkit-scrollbar": {
+      width: "8px",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: "transparent",
+      borderRadius: "20px",
+    },
+    "&::-webkit-scrollbar-track": {
+      backgroundColor: "transparent",
+    },
   },
   mainPaper: {
     width: '100%',
-    display: 'grid',
     display: "flex",
     flexDirection: "row",
     height: "94vh",
-    // gridTemplateColumns: 'repeat(auto-fit, minmax(320px, 1fr))',
-    // gap: theme.spacing(3),
-    //  padding: theme.spacing(2),
-    // marginBottom: theme.spacing(3),
     flex: "2",
   },
   helpPaper: {
     position: 'relative',
     width: '100%',
-    // maxHeight: '100vh',
-    // padding: theme.spacing(2),
     boxShadow: theme.shadows[3],
     borderRadius: theme.spacing(1),
     cursor: 'pointer',
@@ -92,20 +103,21 @@ const useStyles = makeStyles(theme => ({
   },
   videoThumbnail: {
     width: '70%',
-    
-    // height: 'calc(100% - 56px)',
     objectFit: 'cover',
     borderRadius: `${theme.spacing(1)}px ${theme.spacing(1)}px 0 0`,
   },
   videoTitle: {
     marginTop: theme.spacing(1),
     flex: 1,
-    display: "block"
+    display: "block",
+    fontSize: "1rem",
+    fontWeight: "520",
+    color: "black"
   },
   videoDescription: {
     overflow: 'hidden',
     paddingBottom: "250px",
-
+    textAlign: "center",
   },
   videoModal: {
     display: 'flex',
@@ -124,31 +136,14 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-document.addEventListener("DOMContentLoaded", function() {
-  const links = document.querySelectorAll(".help-links a");
-  const contentItems = document.querySelectorAll(".content-item");
-
-  links.forEach(link => {
-      link.addEventListener("click", function(event) {
-          event.preventDefault(); // Impede o comportamento padrão do link
-
-          const targetId = this.getAttribute("data-content");
-
-          // Remove a classe 'active' de todos os itens de conteúdo
-          contentItems.forEach(item => item.classList.remove("active"));
-
-          // Adiciona a classe 'active' ao item de conteúdo correspondente
-          document.getElementById(targetId).classList.add("active");
-      });
-  });
-});
-
 const Helps = () => {
   const classes = useStyles();
   const [records, setRecords] = useState([]);
   const { list } = useHelps();
   const [selectedVideo, setSelectedVideo] = useState(null);
   const [activeContentId, setActiveContentId] = useState(null);
+  const [selectedId, setSelectedId] = useState(null);
+  const [searchTerm, setSearchTerm] = useState(""); // Estado para o termo de pesquisa
 
   useEffect(() => {
     async function fetchData() {
@@ -156,17 +151,25 @@ const Helps = () => {
       setRecords(helps);
     }
     fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const openVideoModal = (video) => {
     setSelectedVideo(video);
   };
-
   const closeVideoModal = () => {
     setSelectedVideo(null);
   };
-//COMANDO ESC PARA FECHAR VIDEO
+
+  // Manipulador de busca
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // Filtra os registros com base no termo de busca
+  const filteredRecords = records.filter(record =>
+    record.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   const handleModalClose = useCallback((event) => {
     if (event.key === "Escape") {
       closeVideoModal();
@@ -179,6 +182,7 @@ const Helps = () => {
       document.removeEventListener("keydown", handleModalClose);
     };
   }, [handleModalClose]);
+
   const renderVideoModal = () => {
     return (
       <Modal
@@ -204,18 +208,18 @@ const Helps = () => {
 
   const renderHelps = () => (
     <div className={`${classes.mainPaper} ${classes.mainPaperContainer}`}>
-      {records.map((record, key) => (
-        activeContentId === record.id && ( // Mostra somente o conteúdo ativo
-          <section key={key} className="content-item active" style={{width: "80%", justifyContent: "center"}}>
+      {filteredRecords.map((record, key) => ( // Usa filteredRecords para exibir os itens filtrados
+        activeContentId === record.id && (
+          <section key={key} className="content-item active" style={{width: "100%", justifyContent: "center"}}>
             <Typography style={{margin: "auto"}} variant="h4" className={classes.titleItem}>{record.title}</Typography>
             <Typography style={{fontSize: "1rem"}} variant="text" className={classes.videoDescription}>{record.description}</Typography>
             <div style={{display: "flex", justifyContent: "center", alignContent: "center", marginTop: "30px"}}>
-             <img
-              src={`https://img.youtube.com/vi/${record.video}/mqdefault.jpg`}
-              alt="Thumbnail"
-              className={classes.videoThumbnail}
-              onClick={() => openVideoModal(record.video)}
-              style={{margin: "auto"}}
+              <img
+                src={`https://img.youtube.com/vi/${record.video}/mqdefault.jpg`}
+                alt="Thumbnail"
+                className={classes.videoThumbnail}
+                onClick={() => openVideoModal(record.video)}
+                style={{margin: "auto"}}
               /> 
             </div>
           </section>
@@ -224,79 +228,60 @@ const Helps = () => {
     </div>
   );
 
-            // <Paper key={key} className={`${classes.helpPaper} ${classes.paperHover}`} onClick={() => openVideoModal(record.video)}>
-            //   <img
-            //     src={`https://img.youtube.com/vi/${record.video}/mqdefault.jpg`}
-            //     alt="Thumbnail"
-            //     className={classes.videoThumbnail}
-            //   />
-            //   <Typography variant="button" className={classes.videoTitle}>
-            //     {record.title}
-            //   </Typography>
-            //   <Typography variant="caption" className={classes.videoDescription}>
-            //     {record.description}
-            //   </Typography>
-            // </Paper>
-          // )) : null}
-  //       </div>
-  //     </>
-  //   );
-  // };
-
   return (
     <div style={{
-      // height: "100%",
       margin: "auto",
       display: "flex",
       width: "90%",
       fontFamily: "Nunito, Arial, sans-serif",
       gap: "1.2rem"
-      }}> 
+    }}> 
       <aside className={classes.sidebarContainer} style={{width: "28%", display: "block", justifyContent: "center", maxHeight: "200px"}}>
-      <MainHeader style={{display: "flex", flexDirection: "column"}}>
-        <Typography style={{fontWeight: "bold", fontSize: "1.875rem", paddingBottom: "0px"}} variant="text">{i18n.t("helps.title")}</Typography>
-        <MainHeaderButtonsWrapper></MainHeaderButtonsWrapper>
-      </MainHeader>
-      <div style={{backgroundColor: "#EFF3F6"}} >
-      <Typography style={{fontWeight: "bold", fontSize: "1.5rem", display: "block", textAlign: "left", paddingLeft: "1rem", paddingTop: "18px"}} variant="text">Pesquise a sua pergunta</Typography>
-      <Typography style={{fontWeight: "500px", fontSize: "1rem", display: "block", textAlign: "left", paddingLeft: "1rem", paddingBottom: "10px", }} variant="text">Caso não encontre, pode pesquisar na barra abaixo:</Typography>
-        <TextField
-            // fullWidth
+        <MainHeader style={{display: "flex", flexDirection: "column"}}>
+          <Typography style={{fontWeight: "bold", fontSize: "1.875rem", paddingBottom: "0px"}} variant="text">{i18n.t("helps.title")}</Typography>
+          <MainHeaderButtonsWrapper></MainHeaderButtonsWrapper>
+        </MainHeader>
+        <div style={{backgroundColor: "#EFF3F6"}}>
+          <Typography style={{fontWeight: "bold", fontSize: "1.5rem", display: "block", textAlign: "left", paddingLeft: "1rem", paddingTop: "18px"}} variant="text">Pesquise a sua pergunta</Typography>
+          <Typography style={{fontWeight: "500px", fontSize: "1rem", display: "block", textAlign: "left", paddingLeft: "1rem", paddingBottom: "10px"}} variant="text">Caso não encontre, pode pesquisar na barra abaixo:</Typography>
+          <TextField
             variant="standard"
-            // size="large"
             className={classes.searchInput}
-            placeholder={"Pesquisar..."}  
+            placeholder="Pesquisar..."
+            value={searchTerm} // Adiciona o valor de searchTerm ao campo de pesquisa
+            onChange={handleSearchChange} // Chama handleSearchChange ao digitar
             InputProps={{
               disableUnderline: true,
-              
               endAdornment: (
                 <InputAdornment position="end">
                   <SearchIcon style={{ color: "gray" }} />
                 </InputAdornment>
               ),
             }}
-            />
-      </div>
-      <div className={classes.titleList}>
-      {records.map((record) => (
-        <nav key={record.id} style={{ display: "block" }}>
-            <div style={{ borderRadius: "10px", padding: "11px", width: "100%", cursor: "pointer"}}>
-              <Typography
-                variant="text"
-                className={classes.videoTitle}
-                onClick={() => setActiveContentId(record.id)  } // Atualiza o conteúdo ativo ao clicar
+          />
+        </div>
+        <div className={classes.titleList}>
+          {filteredRecords.map((record) => ( // Usa filteredRecords para mostrar apenas os itens filtrados na barra lateral
+            <nav key={record.id} style={{ display: "block" }}>
+              <div style={{ borderRadius: "10px", padding: "11px", width: "100%", cursor: "pointer", backgroundColor: selectedId === record.id ? "#BEBEBE" : "transparent", display: "flex", alignItems: "center"}}>
+                <Typography
+                  variant="text"
+                  className={classes.videoTitle}
+                  onClick={() => {
+                    setActiveContentId(record.id);
+                    setSelectedId(record.id);  
+                  }}
                 >
-                <a href="#" style={{ color: "#0c2340", textDecoration: "none" }}>{record.title}</a>
-              </Typography>
-            </div>
-          </nav>
-        ))}
-      </div>
+                  <a href="#" style={{ color: "#0c2340", textDecoration: "none" }}>{record.title}</a>
+                </Typography>
+              </div>
+            </nav>
+          ))}
+        </div>
       </aside>
       <div className={classes.mainPaper}>
         {renderHelps()}
       </div>
-      {/* rendervideomodal abre o video ao clicar */}
       {renderVideoModal()}
     </div>
   );
